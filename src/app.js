11e3950a9b053518387a7afde256dac43eba4de8
hemanthkumarkrishnan <hemanthkrishna944@gmail.com>
2026-01-07 13:25:36 +0530

    dynamic data handling in express request from signup api

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json())
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "akshay",
-    lastName: "saini",
-    emailId: "namste@gmail.com",
-    password: "devtinder",
-  });
+  const user = new User(req.body);
+  console.log(req.body)
   try {
     await user.save();
     res.send("user added sucessesful");
