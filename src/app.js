663a68b4e8d59e905832973791b14d999bde957c
hemanthkumarkrishnan <hemanthkrishna944@gmail.com>
2026-02-03 20:32:23 +0530

    all set for production

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ const express = require("express");
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http")
+
+const app = express();
+
+
 require("dotenv").config();
 require("./utils/cornjob");
-const app = express();
+
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -16,13 +23,20 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
-app.use("/", authRouter, profileRouter, requestRouter, userRouter);
+app.use("/", authRouter, profileRouter, requestRouter, userRouter,chatRouter);
+
+
+const server = http.createServer(app)
+
+initializeSocket(server)
 
 connectDB()
   .then(() => {
     console.log("DB connected");
-    app.listen(3000, () => {
+    server.listen(3000, () => {
       console.log("Server is up and established on port 3000");
     });
   })
